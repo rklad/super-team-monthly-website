de055976bbc3675f0b046088cb6689d45d7ec198
rklad <525rkladerman@frhsd.com>
2024-05-07 14:46:22 -0400

    finished template

--- script.js
+++ script.js
@@ -13,25 +13,76 @@ const teamMembers = [
     },
 ]
 
-function generateTeamCards(){
-    
+function generateTeamCards() {
+
     const teamCardsContainer = document.getElementById('teamCards')
 
     teamMembers.forEach(member => {
         const card = document.createElement('div')
         card.classList.add('col-md-4')
+
+        let backgroundColor
+
+        switch (member.position.toLowerCase()) {
+            case 'quarterback':
+                backgroundColor = 'green'
+                break
+            case 'running back':
+                backgroundColor = 'green'
+                break
+            case 'running back':
+                backgroundColor = 'green'
+                break
+            case 'wide receiver':
+                backgroundColor = 'green'
+                break
+            case 'tight end':
+                backgroundColor = 'green'
+                break
+            case 'offensive line':
+                backgroundColor = 'green'
+                break
+            case 'defensive line':
+                backgroundColor = 'green'
+                break
+            case 'linebacker':
+                backgroundColor = 'green'
+                break
+            case 'cornerback':
+                backgroundColor = 'green'
+                break
+            case 'safety':
+                backgroundColor = 'green'
+                break
+            case 'kicker':
+                backgroundColor = 'green'
+                break
+            case 'punter':
+                backgroundColor = 'green'
+                break
+            case 'coach':
+                backgroundColor = 'green'
+                break
+        }
+
+        card.style.backgroundColor = backgroundColor
+
+
+
+        
+
         card.innerHTML = `
         <div class="card">
             <div class = "card-header">
                 ${member.name}
             </div>
-            <div class = "card-body">
+            <div class = "card-body" style = "background-color:${backgroundColor};">
                 <p><strong>Position:</strong> ${member.position}</p>
             </div>
         </div>
         
         `
-    teamCardsContainer.appendChild(card)
+        teamCardsContainer.appendChild(card)
     });
 }
 
